fb58e0732dfb881b6e22f8252ffc4068e6e04bab
Tove <tove.brant@gmail.com>
2016-11-25 21:19:51 +0200

    seasonChanger connected to seasonObject

--- SeasonsOfLife/Assets/Scripts/SeasonChanger.cs
+++ SeasonsOfLife/Assets/Scripts/SeasonChanger.cs
@@ -1,13 +1,28 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class SeasonChanger : MonoBehaviour {
 
-    public GameObject[] mySeasons;
+    public GameObject[] mySeasonsGraphics;
     public Season CurrentSeason;
+
+    public List<SeasonObject> mySeasonObjects;
+    public GameObject GameObjectHolder;
+
+
+    public void InitiateSeasonObjects()
+    {
+        for(int i = GameObjectHolder.transform.childCount; i > 0; i--)
+        {
+            mySeasonObjects.Add(GameObjectHolder.transform.GetChild(i).GetComponent<SeasonObject>());
+        }
+    }
 
     public void ChangeSeason(Season toSeason)
     {
-
+        foreach(SeasonObject so in mySeasonObjects)
+        {
+            so.ChangeSeason(toSeason);
+        }
     }
 }
