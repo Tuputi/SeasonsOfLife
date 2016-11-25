8fed311de8ed94918a6ae12d26ee6c81f54c456b
Tove <tove.brant@gmail.com>
2016-11-25 22:42:33 +0200

    character has flower

---
+++ SeasonsOfLife/Assets/Scripts/CharacterSeasons.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSeasons : SeasonObject {
+
+    public override void _HandleSeasonChange(Season newSeason)
+    {
+        for (int i = 3; i >= 0; i--)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+        transform.FindChild(newSeason.ToString()).gameObject.SetActive(true);
+        transform.SetAsFirstSibling();
+    }
+}
