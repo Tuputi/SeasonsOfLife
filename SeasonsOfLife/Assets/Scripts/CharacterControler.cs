14927fa1b0d6b60183b67133712deab91d8e48cf
AMAR Anis <ea_amar@esi.dz>
2016-11-26 09:08:14 +0100

    Chara moving not animated

---
+++ SeasonsOfLife/Assets/Scripts/CharacterControler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControler : MonoBehaviour {
+
+    float maxSpeed = 10f;
+    bool facingRight = true; 
+	// Use this for initialization
+	void Start () {
+
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        float move = Input.GetAxis("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(maxSpeed * move, GetComponent<Rigidbody2D>().velocity.y);
+	
+	}
+}
